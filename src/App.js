5d52c6244c61bed018e904160ffb76ec88f7a54a
Landon Phillips <landonphillips@landon.com>
2022-09-09 17:31:26 -0500

    working fetch from xrpl

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 
-import React from 'react'
+import {React, useState } from 'react'
 import './App.css';
 // import xrpl from "xrpl";
 
 
 const xrpl = require("xrpl")
-async function main() {
-  const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
-  await client.connect()
 
-  const response = await client.request({
-    "command": "account_info",
-    "account": "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
-    "ledger_index": "validated"
-  })
-  console.log(response)
 
-  client.disconnect()
-}
-main()
 function App() {
+  const [xrpData, setxrpData] = useState(0)
+
+  async function main() {
+    const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
+    await client.connect()
+  
+    const response = await client.request({
+      "command": "account_info",
+      "account": "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
+      "ledger_index": "validated"
+    })
+    console.log(response)
+    setxrpData(response.result.account_data.Balance)
+  
+    client.disconnect()
+  }
+  main()
+
+
   return (
+    <div>
    <h1>my first xrpl App</h1>
+   <p>Balance: {xrpData}</p>
+   </div>
   );
 }
 
